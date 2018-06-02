47e54f4715d6b6395ece17754604e1d6d280ac1b
Lelana <cfsfresno@gmail.com>
2018-06-02 16:07:15 -0600

    booking.js revision

--- client/src/App/Bookings.js
+++ client/src/App/Bookings.js
@@ -6,15 +6,18 @@ import BookingForm from './BookingForm';
 
 function Bookings(props) {
   return (
+    <div id="booking-page">
     <div className="booking-bkgrd">
-      <div className="video-background">
-    <div className="video-foreground">
+    {/* <div className="booking-bkgrd">
+      <div className="video-background"> */}
+    {/* <div className="video-foreground">
       <iframe  title="earth"src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ" frameBorder="0" allowFullscreen></iframe>
      
-    </div>
-  </div>
+    </div> */}
+ 
 
  <BookingForm/>
+  </div>
   </div>
 
      
@@ -22,6 +25,35 @@ function Bookings(props) {
 }
 
 export default Bookings;
+
+
+
+
+
+// import React from 'react';
+// // import "../styles.css";
+// import BookingForm from './BookingForm';
+ 
+
+
+// function Bookings(props) {
+//   return (
+//     <div className="booking-bkgrd">
+//       <div className="video-background">
+//     <div className="video-foreground">
+//       <iframe  title="earth"src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ" frameBorder="0" allowFullscreen></iframe>
+     
+//     </div>
+//   </div>
+
+//  <BookingForm/>
+//   </div>
+
+     
+//   )
+// }
+
+// export default Bookings;
 
 
 
