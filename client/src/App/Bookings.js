7862299d6399e5f5b552990878a766aa0f3387f6
Lelana <cfsfresno@gmail.com>
2018-06-01 21:47:13 -0600

    revision/reinstall

--- client/src/App/Bookings.js
+++ client/src/App/Bookings.js
@@ -6,15 +6,18 @@ import BookingForm from './BookingForm';
 
 function Bookings(props) {
   return (
+    <div id="booking-page">
     <div className="booking-bkgrd">
-      <div className="video-background">
-    <div className="video-foreground">
+    {/* <div className="booking-bkgrd">
+      <div className="video-background"> */}
+    {/* <div className="video-foreground">
       <iframe  title="earth"src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ" frameBorder="0" allowFullscreen></iframe>
      
-    </div>
-  </div>
+    </div> */}
+ 
 
  <BookingForm/>
+  </div>
   </div>
 
      
